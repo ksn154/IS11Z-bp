420f6aeae34ed1786a6000f53ca899483e084e13
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 19:11:23 +0300

    номер 9

---
+++ bank.pas
@@ -0,0 +1,19 @@
+﻿program bank;
+var r, w, m: real;
+    i, n: integer;
+begin
+  i:=1;
+  write('Introduceci suma: ');
+  readln(r);
+  write('Introduceci procentul lunar: ');
+  readln(w);
+  write('Introduceci luni: ');
+  readln(n);
+  w:=w/100;
+  for i:= 1 to n do
+    begin
+      m:=r*w;
+      r:=r+m;
+    end;
+  write('Suma: ',r:3:3);
+end.
