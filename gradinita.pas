b29b037b2cc29cb9df3e49f29e122ef49d86b35c
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 18:34:03 +0300

    номер 4

---
+++ gradinita.pas
@@ -0,0 +1,13 @@
+﻿program chet;
+var s, n, r, d, o:real;
+begin
+  write('Introduceci plata pe luna: ');
+  readln(s);
+  write('Introduceci nr. de zile fregventate: ');
+  readln(n);
+  write('Introduceci nr. de zile lucratoare: ');
+  readln(r);
+  d:=s/r;
+  o:=n*d;
+  write('Plata voastra: ',o:3:3);
+end.
