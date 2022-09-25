4da07055555be99a0fe09384262f47e1879174fb
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 19:51:43 +0300

    номер 12

---
+++ 3 numere.pas
@@ -0,0 +1,10 @@
+﻿program bank;
+var a, b, c, s, d: real;
+begin
+  write('Introduceci trei numere reale: ');
+  readln(a, b, c);
+  s:=(a+b+c)/3;
+  d:=s/b;
+  writeln('Media aritmetică : ',s);
+  writeln('Impartirea la al doilea numar: ',d);
+end.
