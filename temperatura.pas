72dc12c45ad9fed407825e29052130d949bd01aa
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 19:25:56 +0300

    номер 10

---
+++ temperatura.pas
@@ -0,0 +1,11 @@
+﻿program bank;
+var f, k: real;
+    t: integer;
+begin
+  write('Introduceci temperatura: ');
+  readln(t);
+  f:= t*1.8+32;
+  k:= 273.15+t;
+  writeln('Temperatura Farengeit: ',f);
+  writeln('Temperatura Kelvin: ',k);
+end.
