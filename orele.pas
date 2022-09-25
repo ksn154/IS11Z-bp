b97fba84a2ab2937abd103c486007daec65f04af
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 20:13:16 +0300

    номер 15

---
+++ orele.pas
@@ -0,0 +1,13 @@
+﻿program lectia;
+const i=45;
+var n, m, s, c, v: integer;
+begin
+  write('ora inceperii prin space: ');
+  readln(n, m);
+  n:=n*60;
+  m:=m+n;
+  s:=m+i*6+2*15+3*10;
+  c:=s div 60;
+  v:=s mod 60;
+  write('Lectia a sasea se termina la ',c,':',v);
+end.
