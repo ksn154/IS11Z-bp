d66d85e11aecbb7fa7e788b0aa2894776fb0d6b1
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 18:46:56 +0300

    номер 8

---
+++ calculatoare.pas
@@ -0,0 +1,10 @@
+﻿program monitor;
+var m, b, t, i, o, n, s:real; 
+begin
+  write('Introduceci preţul unui monitor, bloc desistem, tastieră,mouseşi imprimantă: ');
+  readln(m, b, t, i, o);
+  write('Intoduceti numarul de producte: ');
+  readln(n);
+  s:=n*(m+b+t+i+o);
+  write('Plata: ',s);
+end.
