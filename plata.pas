8933fe54e58c9886258e5f9edc0420eea4bd6755
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 18:38:02 +0300

    номер 7

---
+++ plata.pas
@@ -0,0 +1,15 @@
+﻿program mincare;
+const k=36.5; 
+      p=24.4;
+      b=6.7;
+var x, y, z, v:real; 
+begin
+  write('Introduceci kilograme de bomboane: ');
+  readln(x);
+  write('Introduceci kilograme de prajituri:');
+  readln(y);
+  write('Introduceci kilograme de mere: ');
+  readln(z);
+  v:=k*x+p*y+b*z;
+  write('Plata: ',v);
+end.
