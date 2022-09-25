d8218a73fa8d59569219eba84283e0ad64d6c851
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 18:33:40 +0300

    номер 3

---
+++ chet v banke.pas
@@ -0,0 +1,14 @@
+﻿program chet;
+var p, S, x, m, Sum:real;
+begin
+  write('Introduceci procentul: ');
+  readln(p);
+  write('Introduceci summa: ');
+  readln(S);
+  write('Introrduceci numarul de ani: ');
+  readln(x);
+  p:=p/100;
+  m:=S*p;
+  Sum:=x*m+S;
+  write('Bani la cont: ',Sum);
+end.
