3078c1874a129b026b4704074f9a01a41753ef28
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 19:58:43 +0300

    номер 13

---
+++ auto.pas
@@ -0,0 +1,12 @@
+﻿program masina;
+var n, k, x, m: real;
+begin
+  write('Introduceci pretul automodilului vechi: ');
+  readln(n);
+  write('Introduceci suma pe card: ');
+  readln(k);
+  write('Introduceci pretul automobilului nou: ');
+  readln(x);
+  m:=x-k-n;
+  writeln('Suma: ',abs(m));
+end.
