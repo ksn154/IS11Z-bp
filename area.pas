7a8a57c86ed0a4ea65916cbe665ef5868ae69355
Xenia Popovici <114051824+ksn154@users.noreply.github.com>
2022-09-25 18:37:36 +0300

    номер 6

---
+++ area.pas
@@ -0,0 +1,13 @@
+﻿program raznita;
+var a, b, c, p, s:real;
+begin
+  write('Introduceci lungimea: ');
+  readln(a);
+  write('Introduceci latimea: ');
+  readln(b);
+  write('Introduceci inaltimea: ');
+  readln(c);
+  s:=2*(a*c)+2*(b*c);
+  p:=a*b+s;
+  write('Area: ',p);
+end.
